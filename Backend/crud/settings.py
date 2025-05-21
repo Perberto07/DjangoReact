6c5a94813a4e5bd129261702f4f4dbadd3ea7dcb
Perberto07 <164364781+Perberto07@users.noreply.github.com>
2025-05-21 21:06:19 +0800

    up

--- Backend/crud/settings.py
+++ Backend/crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sjr4bg%j9-xc)$dlyojy5v(dn#%c85y8!ieksdd()&kp@u@kop
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.139', '192.168.68.11']
+ALLOWED_HOSTS = ['192.168.0.139', '192.168.68.11', '127.0.0.1']
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
