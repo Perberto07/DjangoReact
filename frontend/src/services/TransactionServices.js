83b410d833213e9430ec745272f37be1ea2584e4
Perberto07 <164364781+Perberto07@users.noreply.github.com>
2025-07-03 15:38:34 +0800

    frontend

--- frontend/src/services/TransactionServices.js
+++ frontend/src/services/TransactionServices.js
@@ -2,10 +2,37 @@ import axios from 'axios';
 
 const API_URL = 'http://127.0.0.1:8000/transaction/'; // Change to your actual API URL
 
-export const getTransaction = async () => {
-  const response = await axios.get(API_URL);
+
+const getAuthHeaders = () => {
+  const token = localStorage.getItem('access_token');
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+};
+
+export const getTransaction = async (page = 1, pageSize = 10) => {
+  const token = localStorage.getItem('access_token');
+  const response = await axios.get(API_URL, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    params: { page, page_size: pageSize },
+  });
+
+  return {
+    results: response.data.results || [],
+    count: response.data.count || 0,
+  };
+};
+
+// TransactionServices.js
+export const getTransactionById = async (id) => {
+  const response = await axios.get(`${API_URL}${id}/`, getAuthHeaders());
   return response.data;
 };
+
 
 export const createTransaction = async (productData) => {
   const response = await axios.post(API_URL, productData);
@@ -13,7 +40,7 @@ export const createTransaction = async (productData) => {
 };
 
 export const updateTransaction= async (id, productData) => {
-  const response = await axios.put(`${API_URL}${id}/`, productData); // <- Add trailing slash
+  const response = await axios.put(`${API_URL}${id}/`, productData, getAuthHeaders()); // <- Add trailing slash
   return response.data;
 };
 
