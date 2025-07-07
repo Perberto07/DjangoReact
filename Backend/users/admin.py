30305acd5bf6413fb0d666901a3e9f4bf5734da3
Perberto07 <164364781+Perberto07@users.noreply.github.com>
2025-07-07 19:02:04 +0800

    api

--- Backend/users/admin.py
+++ Backend/users/admin.py
@@ -1,4 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
-# Register your models here.
-admin.site.register(User)
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email',)
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('email',)
+
+class CustomUserAdmin(BaseUserAdmin):
+    model = User
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ('email', 'is_staff', 'is_active')
+    list_filter = ('email', 'is_staff', 'is_active')
+    ordering = ('email',)
+    search_fields = ('email',)
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+admin.site.register(User, CustomUserAdmin)
